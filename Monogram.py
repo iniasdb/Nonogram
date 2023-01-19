7bf24a665b1fe245ccb7998c184096c0a92e2c97
iniasdb <inias.debelder@hotmail.be>
2023-01-19 23:55:46 +0100

    initial commit

---
+++ Monogram.py
@@ -0,0 +1,152 @@
+from PIL import Image
+
+class Monogram:
+    def __init__(self, verbose):
+        self._verbose = verbose
+        self._rows = []
+        self._cols = []
+
+    def generate(self, width, height, image):
+        self._width = width
+        self._height = height
+        self._image = image
+        self._coords = [["_" for x in range(height)] for y in range(width)]
+
+        self.pixelatedImg = self.convert_image()
+        self.count_adjacent_col()
+        self.count_adjacent_row()
+
+    def convert_image(self) -> Image:
+        # Open image
+        img = Image.open(self._image)
+
+        # Convert image to black and white
+        # Dither none --> no noise
+        img = img.convert('1', dither=Image.NONE)
+
+        # Resize down to 16x16 pixels
+        imgSmall = img.resize((self._width, self._height), resample=Image.Resampling.BILINEAR)
+        return imgSmall
+
+    def count_adjacent_col(self):
+        # Create pixel map
+        pixel_map = self.pixelatedImg.load()
+        # Calc consecutive black squares/row
+        cols = []
+
+        for i in range(self._height):
+            counter = 0
+            col = []
+            for j in range(self._width):
+                if pixel_map[i,j] == 0:
+                    counter+=1
+                else:
+                    if counter > 0:
+                        col.append(counter)
+                        counter = 0
+            cols.append(col)
+        self._cols = cols
+
+        if self._verbose:
+            # print consecutive ...
+            for i in cols:
+                print(i)
+    
+    def count_adjacent_row(self):
+        # Create pixel map
+        img = self.pixelatedImg.rotate(90)
+        pixel_map = img.load()
+        # Calc consecutive black squares/row
+        rows = []
+
+        for i in range(self._height):
+            counter = 0
+            row = []
+            for j in range(self._width):
+                if pixel_map[i,j] == 0:
+                    counter+=1
+                else:
+                    if counter > 0:
+                        row.append(counter)
+                        counter = 0
+            rows.append(row)
+        
+        self._rows = rows
+
+        if self._verbose:
+            # print consecutive ...
+            for i in rows:
+                print(i)
+
+    def print_puzzle(self):
+        # Find biggest list in cols
+        maxCols = 0
+        for col in self._cols:
+            if len(col) > maxCols:
+                maxCols = len(col)
+        
+        # Find biggest list in rows
+        maxRows = 0
+        for row in self._rows:
+            if len(row) > maxRows:
+                maxRows = len(row)
+
+        ###
+        # loop between 0 and max
+        # loop cols
+        # print index 0 of the biggest list first, else print " "
+        # after looping cols, new line
+        # print index 0 of second biggest list...
+        # and so on
+        # the smallest list: 0 (i == max-1), print ~
+        ###
+        cols = self._cols
+        cols.reverse()
+        for i in range(maxCols):
+            # ofset because of rows
+            print("   "*maxRows, end="")
+
+            for col in cols:
+                if len(col) == maxCols-i:
+                    print(f" {col[0]} ", end="")
+                    col.pop(0)
+                else:
+                    if i == maxCols-1:
+                        print(" ~ ", end="")
+                    else:
+                        print("   ", end="")
+            print()
+
+        ###
+        # Loop rows
+        # If len() = 0 (no black squares), print " " ofset and ~
+        # Else print ofset and number
+        ###
+        rows = self._rows
+        for row in rows:
+            if len(row) == 0:
+                print("   "*(maxRows-1), end="")
+                print(" ~ ", end="")
+            else:
+                print("   "*(maxRows-len(row)), end="")
+                for n in row:
+                    print(f" {n} ", end="")
+            print()
+
+    def print_image(self):
+        # Create pixel map
+        img = self.pixelatedImg.rotate(90)
+        pixel_map = img.load()
+
+        # Loop over pixels
+        for i in range(self._height):
+            for j in range(self._width):
+                if pixel_map[i,j] == 0:
+                    self._coords[i][j] = "x"
+
+        # Print coordsmap
+        for i in range(self._height):
+            for j in range(self._width):
+                print(self._coords[i][j], end="")
+            print()
+
